532c4f1053337ab50a5f6bb0b8011590abefe597
agusbektir <agusbektir@gmail.com>
2020-06-30 15:37:49 +0700

    add fs.readFile to server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const http = require('http');
 
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set heder content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<p>Hello, world!</p>');
-    res.write('<p>Hello, world again!</p>');
-    res.end();
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
 });
 
 server.listen(8000, 'localhost', () => {
