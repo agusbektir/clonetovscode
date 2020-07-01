a6783f42b013e57db21189a21324758d5443d309
agusbektir <agusbektir@gmail.com>
2020-07-01 09:31:39 +0700

    add npm

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
-
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
@@ -20,7 +20,7 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200;
             break;
-        case '/about-me':
+        case '/about-us':
             res.statusCode = 301;
             res.setHeader('Location', '/about');
             res.end();
